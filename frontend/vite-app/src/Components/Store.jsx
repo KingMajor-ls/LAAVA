9c673b944c808d36831a41c959c1a82897e21593
Tsepang Nkoe <nkoetsepang323@gmail.com>
2024-05-13 15:42:57 +0200

    updates UI

--- frontend/vite-app/src/Components/Store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from './Reducer';
-
-// Create the Redux store
-const Store = configureStore({
-  reducer: reducer,
-});
-
-export default Store;
