1aeb56ce44e6ff26f04eeef4ddefe86f4df172a4
Tsepang Nkoe <nkoetsepang323@gmail.com>
2024-05-16 07:27:48 +0200

    header enhancement

--- frontend/vite-app/src/Components/Header.jsx
+++ frontend/vite-app/src/Components/Header.jsx
@@ -1,12 +1,13 @@
-// import React from 'react'
-import '../Styles/App.css'
+// Header.jsx
+import '../Styles/App.css';
 
 function Header() {
   return (
-    <div className='laava'>
-        <h1>LAAVA</h1>
+    <div className="laava">
+      <h1>LAAVA</h1>
+      <div className="ai-sparks"></div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
